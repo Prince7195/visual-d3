bb155d3bc6fab0a82ca96b503426dc22f9d858de
Vijay Deepak <vijaydeepak7195@outlook.com>
2019-11-21 23:16:02 +0530

    bar chart started

--- bar-chart/index.js
+++ bar-chart/index.js
@@ -1,13 +1,31 @@
-const { select, csv } = d3;
+const { select, csv, scaleLinear, max } = d3;
+// scaleLinear used for quantitative
 
 const width = 960;
 const height = 600;
+let alignHeight = 0;
 
 const svg = select('#bar-chart')
     .append('svg')
       .attr('width', width)
       .attr('height', height);
 
-      csv('/data/population.csv').then(res => {
-        console.log(res);
-      });
+const render = data => {
+  const xScale = scaleLinear()
+     .domain([0, max(data, d => d.population)])
+     .range([0, width]);
+
+  const yScale = '';
+
+  svg.selectAll('rect').data(data)
+     .enter().append('rect')
+     .attr('width', d => xScale(d.population))
+     .attr('height', 30);
+};
+
+csv('/data/population.csv').then(response => {
+  response.forEach(res => {
+    res.population = +res.population * 100
+  });
+  render(response);
+});
