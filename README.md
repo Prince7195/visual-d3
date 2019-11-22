3c4481ae5d90ca6e328d231b4fdfc3c450a390f9
Vijayasekhar Deepak <Vijayasekhar.Deepak@ducenIT.com>
2019-11-22 13:04:02 +0530

    completed bar chart

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # D3
+
+### Attribute type
+1. Categorical
+    * Has distinct different amoung themselves. Ex: Column names.
+2. Ordered
+    * Ordinal
+        * Has distinct thing and has similarity. Ex: shirt in different sizes.
+    * Quantitative
+        * data in numbers.
--- bar-chart/index.js
+++ bar-chart/index.js
@@ -1,9 +1,52 @@
-const { select, csv, scaleLinear, max } = d3;
-// scaleLinear used for quantitative
+const { select, csv, scaleLinear, max, scaleBand, axisLeft, axisBottom, format } = d3;
+// scaleLinear used for quantitative scales
+// scaleBand used for ordinal scales
 
-const width = 960;
+const width = 1000;
 const height = 600;
-let alignHeight = 0;
+const margins = {
+  top: 120,
+  right: 20,
+  bottom: 100,
+  left: 150
+};
+const innerWidth = width - (margins.left + margins.right);
+const innerHeight = height - (margins.top + margins.bottom);
+const style = {
+  fillColor: 'steelblue',
+  fontFamily: 'sans-serif',
+  title: {
+    fontSize: '3em',
+    fontFamily: 'sans-serif',
+  },
+  x: {
+    axisValue: {
+      fontSize: '1em',
+      fontFamily: 'sans-serif',
+    },
+    axisLable: {
+      fontSize: '1em',
+      fontFamily: 'sans-serif',
+      fillColor: 'steelblue',
+      fontWeight: 'bold'
+    },
+    tick: {
+      stroke: '#c7c7c7'
+    }
+  },
+  y: {
+    axisValue: {
+      fontSize: '1em',
+      fontFamily: 'sans-serif',
+    },
+    axisLable: {
+      fontSize: '1em',
+      fontFamily: 'sans-serif',
+      fillColor: 'steelblue',
+      fontWeight: 'bold'
+    },
+  }
+};
 
 const svg = select('#bar-chart')
     .append('svg')
@@ -11,21 +54,73 @@ const svg = select('#bar-chart')
       .attr('height', height);
 
 const render = data => {
+  const xValue = d => d.population;
+  const yValue = d => d.country;
+  const g = svg.append('g')
+     .attr('transform', `translate(${margins.left},${margins.top})`);
   const xScale = scaleLinear()
-     .domain([0, max(data, d => d.population)])
-     .range([0, width]);
+     .domain([0, max(data, xValue)])
+     .range([0, innerWidth]);
 
-  const yScale = '';
+  const yScale = scaleBand()
+     .domain(data.map(yValue))
+     .range([0, innerHeight])
+     .padding(0.1);
 
-  svg.selectAll('rect').data(data)
+  const xScaleFormat = number => format('.3s')(number).replace('G', 'B');
+  const xAxis = axisBottom(xScale)
+   .tickFormat(xScaleFormat)
+   .tickSize(-innerHeight);
+
+  const yAxisG = g.append('g')
+   .call(axisLeft(yScale))
+   .attr('font-size', style.y.axisValue.fontSize);
+
+  yAxisG.selectAll('.domain, .tick line').remove();
+
+  yAxisG.append('text')
+   .text('countries')
+   .attr('fill', style.x.axisLable.fillColor)
+   .attr('font-weight', style.y.axisLable.fontWeight)
+   .attr('y', innerHeight/2)
+   .attr('transform', `rotate(270 -110 ${innerHeight/2})`);
+
+  const xAxisG = g.append('g').call(xAxis)
+   .attr('transform', `translate(0,${innerHeight})`)
+   .attr('font-size', style.x.axisValue.fontSize);
+   
+  xAxisG.select('.domain').remove();
+
+  xAxisG.selectAll('.tick line')
+     .attr('stroke', '#c7c7c7');
+
+  xAxisG.append('text')
+     .text('Population')
+     .attr('fill', style.x.axisLable.fillColor)
+     .attr('font-weight', style.x.axisLable.fontWeight)
+     .attr('x', innerWidth/2)
+     .attr('y', 60);
+     
+
+  const gRect = g.append('g')
+     .attr('fill', style.fillColor);
+
+  gRect.selectAll('rect').data(data)
      .enter().append('rect')
-     .attr('width', d => xScale(d.population))
-     .attr('height', 30);
+     .attr('y', d => yScale(yValue(d)))
+     .attr('width', d => xScale(xValue(d)))
+     .attr('height', yScale.bandwidth());
+  
+  g.append('text')
+   .attr('y', -10)
+   .attr('font-size', style.title.fontSize)
+   .attr('font-family', style.title.fontFamily)
+   .text('Top 10 most populous countries');
 };
 
 csv('/data/population.csv').then(response => {
   response.forEach(res => {
-    res.population = +res.population * 100
+    res.population = +res.population * 1000
   });
   render(response);
 });
